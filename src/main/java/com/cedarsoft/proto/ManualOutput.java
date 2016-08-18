d566140a8529ef2b44e61cee46270d715847798e
Johannes Schneider <js@cedarsoft.com>
2016-08-18 18:01:07 +0200

    add direct buffer

--- src/main/java/com/cedarsoft/proto/ManualOutput.java
+++ src/main/java/com/cedarsoft/proto/ManualOutput.java
@@ -25,6 +25,20 @@ public class ManualOutput {
     for (double value : data.getValues()) {
       byteBuffer.putDouble(value);
     }
+  }
 
+  @Benchmark
+  public void writeByteBufferDirect() throws Exception {
+    Data data = new Data();
+
+    ByteBuffer byteBuffer = ByteBuffer.allocateDirect((data.getMillis().length * 8) + (data.getValues().length * 8));
+
+    for (long value : data.getMillis()) {
+      byteBuffer.putLong(value);
+    }
+
+    for (double value : data.getValues()) {
+      byteBuffer.putDouble(value);
+    }
   }
 }
