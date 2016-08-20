07b141e0b5bb990c68ea9b5f74db132aaa48d056
Johannes Schneider <js@cedarsoft.com>
2016-08-20 13:59:54 +0200

    increase size - cache byte buffers

--- src/main/java/com/cedarsoft/history/storage/Data.java
+++ src/main/java/com/cedarsoft/history/storage/Data.java
@@ -1,18 +1,24 @@
 package com.cedarsoft.history.storage;
 
+import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.State;
+
 /**
  * @author Johannes Schneider (<a href="mailto:js@cedarsoft.com">js@cedarsoft.com</a>)
  */
 public class Data {
+  public static final int COUNT = 10000;
+  public static final int DEPTH = 600;
+
   private final long[] millis;
   private final double[] values;
 
   public Data() {
-     millis = new long[1000];
-     values = new double[1000];
+     millis = new long[DEPTH];
+     values = new double[DEPTH*COUNT];
 
      for (int i=0;i<millis.length;i++) {
-       millis[i] = i * 1000;
+       millis[i] = i * 1531;
      }
      for (int i=0;i<values.length;i++) {
        values[i] = i * 100.5;
--- src/main/java/com/cedarsoft/proto/ManualOutput.java
+++ src/main/java/com/cedarsoft/proto/ManualOutput.java
@@ -1,22 +1,26 @@
 package com.cedarsoft.proto;
 
 import com.cedarsoft.history.storage.Data;
-import com.esotericsoftware.kryo.io.Output;
-import com.esotericsoftware.kryo.io.UnsafeOutput;
+import com.google.common.base.Stopwatch;
 import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.State;
 
-import java.io.ByteArrayOutputStream;
 import java.nio.ByteBuffer;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author Johannes Schneider (<a href="mailto:js@cedarsoft.com">js@cedarsoft.com</a>)
  */
+@State(Scope.Thread)
 public class ManualOutput {
+  private final Data data = new Data();
+  private final ByteBuffer directBuffer = ByteBuffer.allocateDirect(calculateCapacity(data));
+  private final ByteBuffer byteBuffer = ByteBuffer.allocate(calculateCapacity(data));
+
   @Benchmark
   public void writeByteBuffer() throws Exception {
-    Data data = new Data();
-
-    ByteBuffer byteBuffer = ByteBuffer.allocate((data.getMillis().length * 8) + (data.getValues().length * 8));
+    byteBuffer.rewind();
 
     for (long value : data.getMillis()) {
       byteBuffer.putLong(value);
@@ -29,16 +33,34 @@ public class ManualOutput {
 
   @Benchmark
   public void writeByteBufferDirect() throws Exception {
-    Data data = new Data();
-
-    ByteBuffer byteBuffer = ByteBuffer.allocateDirect((data.getMillis().length * 8) + (data.getValues().length * 8));
+    directBuffer.rewind();
 
     for (long value : data.getMillis()) {
-      byteBuffer.putLong(value);
+      directBuffer.putLong(value);
     }
 
     for (double value : data.getValues()) {
-      byteBuffer.putDouble(value);
+      directBuffer.putDouble(value);
     }
+  }
+
+  private static int calculateCapacity(Data data) {
+    return (data.getMillis().length * 8) + (data.getValues().length * 8);
+  }
+
+  public static void main(String[] args) throws Exception {
+    ManualOutput manualOutput = new ManualOutput();
+
+    Stopwatch stopwatch = Stopwatch.createStarted();
+    for (int i = 0; i < 100; i++) {
+      manualOutput.writeByteBufferDirect();
+    }
+    stopwatch.stop();
+
+    System.out.println("10 Took: " + stopwatch.elapsed(TimeUnit.MILLISECONDS));
+    int capacity = calculateCapacity(new Data());
+    System.out.println("Buffer Size: " + capacity + " Bytes");
+    System.out.println("Buffer Size: " + capacity / 1024.0 + " KB");
+    System.out.println("Buffer Size: " + capacity / 1024.0 / 1024.0 + " MB");
   }
 }
